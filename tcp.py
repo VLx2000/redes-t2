d7cec51b39d20871e140e8a2d3e85226eeffe1b3
VLx2000 <victor.antunes.2000@gmail.com>
2021-10-08 18:24:21 -0300

    solucao passo 4

--- tcp.py
+++ tcp.py
@@ -79,16 +79,20 @@ class Conexao:
     def _rdt_rcv(self, seq_no, ack_no, flags, payload):
         # Chame self.callback(self, dados) para passar dados para a camada de aplicação após
         # garantir que eles não sejam duplicados e que tenham sido recebidos em ordem.
-        if seq_no == self.seq_esperado and len(payload) > 0:
+        if seq_no == self.seq_esperado:
             (src_addr, src_port, dst_addr, dst_port) = self.id_conexao
 
-            print('recebido payload: %r' % payload)
-            self.callback(self, payload)
-            self.seq_esperado += len(payload)
-            segmento = fix_checksum(make_header(src_port, dst_port, self.ack_enviado, self.seq_esperado, (FLAGS_ACK)), src_addr, dst_addr) 
-            self.servidor.rede.enviar(segmento, dst_addr)
-        else:
-            self.callback(self, b"")
+            if (flags & FLAGS_FIN) == FLAGS_FIN:
+                self.callback(self, b'')
+                segmento = fix_checksum(make_header(src_port, dst_port, self.ack_enviado, self.seq_esperado + 1, (FLAGS_ACK)), src_addr, dst_addr)
+                self.servidor.rede.enviar(segmento, dst_addr)
+
+            elif len(payload) > 0:
+                print('recebido payload: %r' % payload)
+                self.callback(self, payload)
+                self.seq_esperado += len(payload)
+                segmento = fix_checksum(make_header(src_port, dst_port, self.ack_enviado, self.seq_esperado, (FLAGS_ACK)), src_addr, dst_addr) 
+                self.servidor.rede.enviar(segmento, dst_addr)
 
 
     # Os métodos abaixo fazem parte da API
@@ -104,7 +108,6 @@ class Conexao:
         """
         Usado pela camada de aplicação para enviar dados
         """
-        # TODO: implemente aqui o envio de dados.
         # Chame self.servidor.rede.enviar(segmento, dest_addr) para enviar o segmento
         # que você construir para a camada de rede.
         (src_addr, src_port, dst_addr, dst_port) = self.id_conexao
@@ -133,6 +136,9 @@ class Conexao:
     def fechar(self):
         """
         Usado pela camada de aplicação para fechar a conexão
-        """
-        # TODO: implemente aqui o fechamento de conexão
+        """         
+        (src_addr, src_port, dst_addr, dst_port) = self.id_conexao
+
+        segmento = fix_checksum(make_header(src_port, dst_port, self.ack_enviado, self.seq_esperado + 1, (FLAGS_FIN)), src_addr, dst_addr)
+        self.servidor.rede.enviar(segmento, dst_addr)
         pass
